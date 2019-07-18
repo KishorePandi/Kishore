52cfda0deeed3a82862afd406a90f5009477762f
KishorePandi <53034547+KishorePandi@users.noreply.github.com>
2019-07-18 15:18:18 +0530

    Update Kishore1.py

--- Kishore1.py
+++ Kishore1.py
@@ -1,6 +1,8 @@
-a=int(input('Enter any number: '))
+a=int(input())
 number = a%2
 if number == 0:
-    print(a, "is an even number")
+    print("Even")
+elif number ==1:
+    print("Odd")
 else:
     print("invalid")
